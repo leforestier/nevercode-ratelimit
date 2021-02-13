ac1b7436e2249fa9e3c860e96b7785dfab51599a
Benjamin Le Forestier <7140846+leforestier@users.noreply.github.com>
2021-02-13 21:52:32 +0100

    making RedisSlidingWindowCounter importable directly from ratelimit

--- ratelimit/__init__.py
+++ ratelimit/__init__.py
@@ -1,2 +1,3 @@
 from ratelimit.middleware import RateLimit
 from ratelimit.simple_sliding_window_counter import SimpleSlidingWindowCounter
+from ratelimit.redis_sliding_window_counter import RedisSlidingWindowCounter
